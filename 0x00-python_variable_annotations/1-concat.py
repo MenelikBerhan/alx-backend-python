9c2c7d502ff146766c2514b77df681a60c07a54b
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-11 23:54:17 +0300

    Tasks 0 - 4

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Contains a type-annotated function """
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concats two strings and returns the result."""
+    return str1 + str2
