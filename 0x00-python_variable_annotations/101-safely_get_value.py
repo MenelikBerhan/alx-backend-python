1e87fc40c6fc9673989cde9972f4c615f0fb88f5
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-12 00:47:45 +0300

    Use typevar for default parameter only

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,15 +2,28 @@
 """Contains a type-annotated function """
 from typing import Any, Mapping, TypeVar, Union
 
+K = TypeVar('K')    # type of dct keys
+V = TypeVar('V')    # type of dct values
+T = TypeVar('T')    # type of default
 
-K = TypeVar('K')    # type of keys
-V = TypeVar('V')    # type of values
+
 def safely_get_value(
-        dct: Mapping[K, V], key: K, default: Any = None
-        ) -> Union[V, Any]:
+        dct: Mapping, key: Any, default: Union[T, None] = None
+        ) -> Union[Any, T]:
     """If key is in dct returns its corresponding value,
     else returns default."""
     if key in dct:
         return dct[key]
     else:
         return default
+
+
+# def safely_get_value(
+#         dct: Mapping[K, V], key: K, default: Any = None
+#         ) -> Union[V, Any]:
+#     """If key is in dct returns its corresponding value,
+#     else returns default."""
+#     if key in dct:
+#         return dct[key]
+#     else:
+#         return default
