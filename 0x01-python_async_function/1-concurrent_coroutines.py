9b6574cf497b06678dcab0bc427b20e673a154a4
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-15 21:19:51 +0300

    Remove default value for max_delay param

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns wait_random n times with the specified max_delay.
     Returns the list of all the delays (float values) in ascending order"""
 # delays = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
