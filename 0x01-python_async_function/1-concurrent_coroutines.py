d1c300ef106102b37c3c9713b23fc0eef2ade57d
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-15 21:40:24 +0300

    Clean up

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Contains an asynchronous coroutine"""
 import asyncio
-import random
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -10,12 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns wait_random n times with the specified max_delay.
     Returns the list of all the delays (float values) in ascending order"""
-# delays = await asyncio.gather(*(wait_random(max_delay) for i in range(n)))
-    # return list(delays)
-    # tasks = []
-    # for _ in range(n):
-    #     # tasks.append(asyncio.create_task(wait_random(max_delay)))
-    #     tasks.append(wait_random(max_delay))
+
     delays = []
     for f in asyncio.as_completed([wait_random(max_delay) for _ in range(n)]):
         delay = await f
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Contains an asynchronous coroutine"""
-import asyncio
 import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
@@ -10,7 +9,5 @@ def measure_time(n: int, max_delay: int) -> float:
     """Measures the total execution time for `wait_n(n, max_delay)`,
     and returns `total_time / n`. """
     start = time.perf_counter()
-    # print(asyncio.run(wait_n(n, max_delay)))
     end = time.perf_counter() - start
-    # print(f"Program finished in {end:0.2f} seconds.")
     return 1.0 * end / n
