fbf18e33f730d9b04024c2423ae5dbcccae7e15f
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-01-16 22:25:08 +0300

    Type annotiate return type as Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""Contains an asynchronous coroutine"""
+"""Contains an asynchronous generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """Loops 10 times, each time asynchronously wait 1
     second, then yield a random number between 0 and 10."""
     for _ in range(10):
