daaeb009335266aa6abd00b0f496945f5836e7d9
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-04 02:27:13 +0300

    Task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 """
 from client import GithubOrgClient
 from parameterized import parameterized
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 import unittest
 
 
@@ -19,6 +19,21 @@ class TestGithubOrgClient(unittest.TestCase):
         """Tests the `org` method's return."""
         mocked_get_json.return_value = payload
         client_obj = GithubOrgClient(org_name)
+
+        # `org` method returns `get_json(org_url)`
         self.assertEqual(client_obj.org, payload)
+
         org_url = "https://api.github.com/orgs/{}".format(org_name)
         mocked_get_json.assert_called_once_with(org_url)
+
+    def test_public_repos_url(self):
+        """Tests the property method `_public_repos_url`."""
+        target = 'test_client.GithubOrgClient.org'
+        # `PropertyMock` to mock a property (`org`)
+        with patch(target, new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {"repos_url": "https://test.com"}
+            client_obj = GithubOrgClient('test')
+
+            # `_public_repos_url` returns `org["repos_url"]`
+            self.assertEqual(client_obj._public_repos_url, "https://test.com")
+            mock_org.assert_called_once_with()
