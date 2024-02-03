fad9a96f74bd2c6b61a4bec76ecf6cfc829d0bf9
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-03 23:35:34 +0300

    Task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Parameterized unittests.
 """
-from utils import access_nested_map, get_json
-from parameterized import parameterized, parameterized_class
-from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json, memoize
+from parameterized import parameterized
+from unittest.mock import patch, Mock, DEFAULT
 import unittest
 
 
@@ -45,3 +45,29 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(get_json(url), payload)
         # assert get is called once with the right argument
         mock_req_get.assert_called_once_with(url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Unit test class for utils module.
+    """
+    # @patch(__name__ + '.TestMemoize.test_memoize')
+    def test_memoize(self):
+        """Tests the `memoize` decorator."""
+        class TestClass:
+            """An example class to test the decorator."""
+            def a_method(self):
+                """A method called only once by a memoized method."""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """A memoized method that calls `a_method` once for first
+                run and returns from memory for subsequent calls."""
+                return self.a_method()
+
+        mocked_a_method = patch.object(TestClass, 'a_method').start()
+        mocked_a_method.return_value = 42
+        obj = TestClass()
+        self.assertEqual(obj.a_property, 42)
+        self.assertEqual(obj.a_property, 42)
+        mocked_a_method.assert_called_once()
