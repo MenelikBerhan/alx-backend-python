7dbea0f7bd02bb82be312d289091febd1147202b
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-03 20:23:04 +0300

    Task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Parameterized unittests.
 """
-from utils import access_nested_map
-from parameterized import parameterized
+from utils import access_nested_map, get_json
+from parameterized import parameterized, parameterized_class
+from unittest.mock import patch, Mock
 import unittest
 
 
@@ -27,3 +28,32 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
         self.assertEqual(cm.exception.args[0], err_msg)
+
+
+@parameterized_class(('test_url', 'test_payload'), [
+    ("http://example.com", {"payload": True}),
+    ("http://holberton.io", {"payload": False})
+])
+class TestGetJson(unittest.TestCase):
+    """Unit test class for utils module.
+    """
+    # @parameterized.expand([
+    #     ("http://example.com", {"payload": True}),
+    #     ("http://holberton.io", {"payload": False})
+    # ])
+    # @patch('requests.get')
+    # def test_get_json(self, test_url: str, test_payload: dict,
+    # mock_req_get: Mock):
+    #     """Tests the method for correct return."""
+    #     mock_req_get.return_value.json.return_value = test_payload
+    #     self.assertEqual(get_json(test_url), test_payload)
+    #     mock_req_get.assert_called_once_with(test_url)
+
+    @patch('requests.get')
+    def test_get_json(self, mock_req_get: Mock):
+        """Tests the `get_json` method for correct return."""
+        mock_req_get.return_value.json.return_value = self.test_payload
+        # assert correct return
+        self.assertEqual(get_json(self.test_url), self.test_payload)
+        # assert get is called once with the right argument
+        mock_req_get.assert_called_once_with(self.test_url)
