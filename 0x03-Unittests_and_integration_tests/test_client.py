7554509786b4ad3d9d8152662db520840319367c
Menelik_Berhan <MenelikBerhan@gmail.com>
2024-02-04 03:27:22 +0300

    Task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -37,3 +37,32 @@ class TestGithubOrgClient(unittest.TestCase):
             # `_public_repos_url` returns `org["repos_url"]`
             self.assertEqual(client_obj._public_repos_url, "https://test.com")
             mock_org.assert_called_once_with()
+
+    @patch('client.get_json')
+    def test_public_repos(self, mocked_get_json: Mock):
+        """Tests the `public_repos` method."""
+        # `public_repos` calls `repos_paylod()` which in turn
+        # returns `get_json(_public_repos_url)`
+        test_payload = [
+            {'name': 'a', 'license': {'key': '1'}},
+            {'name': 'b', 'license': {'key': '2'}},
+            {'name': 'c', 'license': {'key': '1'}},
+        ]
+        target = 'test_client.GithubOrgClient._public_repos_url'
+        # `PropertyMock` to mock a property (`_public_repos_url`)
+        with patch(target, new_callable=PropertyMock) as mock_public_repos_url:
+            # set return value for mocked methods
+            mocked_get_json.return_value = test_payload
+            mock_public_repos_url.return_value = "https://test.com"
+
+            # check returns from `public_repos`
+            client_obj = GithubOrgClient('test')
+            self.assertEqual(client_obj.public_repos(), ['a', 'b', 'c'])
+            self.assertEqual(client_obj.public_repos('1'), ['a', 'c'])
+            self.assertEqual(client_obj.public_repos('2'), ['b'])
+            self.assertEqual(client_obj.public_repos('3'), [])
+
+            # since `repos_paylod` is memoized, get_json(_public_repos_url) is
+            # called only once. For the rest, returned from memory.
+            mock_public_repos_url.assert_called_once_with()
+            mocked_get_json.assert_called_once_with("https://test.com")
